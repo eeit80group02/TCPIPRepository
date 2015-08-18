fd2f21a1ac387c9dc840ac8cf726e548a827a593
eeit80 <eeit80@312-PC>
2015-08-18 20:58:50 +0800

    GradestudsBean.java
    增加屬性

---
+++ ProjJDBC/src/model/GradestudsBean.java
@@ -0,0 +1,18 @@
+package model;
+
+public class GradestudsBean {
+	private int schoolId;				// 學校編號
+	private int anniversary;			// 學年度(ex:104)
+	private int elementaryFirst;		// 年級(一年級)
+	private int elementarySecond;		// 年級(二年級)
+	private int elementaryThird;		// 年級(三年級)
+	private int elementaryFourth;		// 年級(四年級)
+	private int elementaryFifth;		// 年級(五年級)
+	private int elementarySixth;		// 年級(六年級)
+	private int juniorFirst;			// 年級(七年級)
+	private int juniorSecond;			// 年級(八年級)
+	private int juniorThird;			// 年級(九年級)
+}
+		
+		
+		
