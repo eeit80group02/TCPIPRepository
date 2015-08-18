3cdfe8f7565d552d4db94fc58a7b12287984cce6
eeit80 <eeit80@312-PC>
2015-08-18 19:46:47 +0800

    MissionSetBean.java
    Add MissionSetBean Field

---
+++ ProjJDBC/src/model/MissionSetBean.java
@@ -0,0 +1,9 @@
+package model;
+
+public class MissionSetBean 
+{
+	private int fullProjId;	     // 完整計畫編號 FK
+	private int missionSetId;    // 任務集編號 PK
+	private String name;         // 任務集名稱
+	private String missionOrder; // 任務集順序
+}
