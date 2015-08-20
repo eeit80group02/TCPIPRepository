bf81fd60b968321f496425517a13e2385f55d6b1
eeit80 <eeit80@312-PC>
2015-08-20 19:22:57 +0800

    OffersBean.java
    Add OffersBean Field

---
+++ ProjJDBC/src/model/OffersBean.java
@@ -0,0 +1,10 @@
+package model;
+
+public class OffersBean 
+{
+	private int planId;       //  計畫需求編號 FK
+	private boolean room;     // 住宿(1表示提供 0 表示不提供)
+	private boolean place;    // 活動場地(1表示提供 0 表示不提供)
+	private boolean food;     // 伙食(1表示提供 0 表示不提供)
+
+}
