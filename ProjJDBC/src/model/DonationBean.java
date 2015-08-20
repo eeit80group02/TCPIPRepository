51f24e5b91633881a8f89ef065e9beb6ff7880c8
eeit80 <eeit80@312-PC>
2015-08-20 20:40:49 +0800

    DonationBean.java
    Update Field

--- ProjJDBC/src/model/DonationBean.java
+++ ProjJDBC/src/model/DonationBean.java
@@ -8,14 +8,19 @@ public class DonationBean
 	private String supplyName;           // 物資名稱
 	private int originalDemandNumber;    // 原始輸入需求數量(數量)
 	private String originalDemandUnit;   // 原始輸入需求數量(單位)	
-	private int DemandNumber;            // 現在需求數量及單位(數量)	
+	private int demandNumber;            // 現在需求數量及單位(數量)	
 	private String size;      	         // 尺寸規格(物品的大小>長*寬*高)	
 	private String demandContent;        // 需求說明(為什麼需要這項物資)
 	private String supplyStatus;         // 物資狀態(全新/二手/不拘)
-	private java.util.Date traiseStart;  // 募集提出需求時間(物品開始募集時間)(即刻上架)
-	private java.util.Date traiseEnd;    // 募集截止時間(物品結束募集時間)(當日0:00下架)
-	private String objPicName;           // 圖片檔名
-	private byte[] objPic;               // 圖片(需要的物品的圖片)
-	private long objPicLength;           // 圖片長度
+	private java.util.Date demandTime;   // 募集提出需求時間(物品開始募集時間)(即刻上架)
+	private java.util.Date expireTime;   // 募集截止時間(物品結束募集時間)(當日0:00下架)
+	private String imageName;            // 圖片檔名
+	private byte[] imageFile;            // 圖片(需要的物品的圖片)
+	private long imageLength;            // 圖片長度
 	private String remark;               // 備註(可以填寫額外的訊息)
 }
+
+
+
+
+
