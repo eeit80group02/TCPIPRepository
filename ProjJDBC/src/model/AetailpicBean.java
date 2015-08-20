f33b24f144a8928bbe08bbee07c69292af7e165d
eeit80 <eeit80@312-PC>
2015-08-20 21:00:22 +0800

    AetailpicBean.java
    Add AetailpicBean Field

---
+++ ProjJDBC/src/model/AetailpicBean.java
@@ -0,0 +1,11 @@
+package model;
+
+public class AetailpicBean 
+{
+	private int fullProjId;            //  計畫編號(完整計畫編號) FK
+	private int imageName;             //  圖片檔名
+	private Byte[] imageFile;          //  圖檔(檔案)
+	private long imageLength;           //  圖片長度(file)
+	private String imageDescribe;         //  圖片敘述
+}
+
