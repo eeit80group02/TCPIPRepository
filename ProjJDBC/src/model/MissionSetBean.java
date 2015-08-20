a997d0309a90ebbfafc92d261941745d9fe200d5
eeit80 <eeit80@312-PC>
2015-08-20 20:10:14 +0800

    MissionBean.java MissionSetBean.java MissionParticipatorBean.java
    Update Field

--- ProjJDBC/src/model/MissionSetBean.java
+++ ProjJDBC/src/model/MissionSetBean.java
@@ -5,5 +5,5 @@ public class MissionSetBean
 	private int fullProjId;	     // 完整計畫編號 FK
 	private int missionSetId;    // 任務集編號 PK
 	private String name;         // 任務集名稱
-	private String missionOrder; // 任務集順序
+	private String missionSetOrder; // 任務集順序
 }
