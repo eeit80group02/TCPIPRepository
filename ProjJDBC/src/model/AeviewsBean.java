eabf24e44307bf988ca6823a8a29d63382eee230
eeit80 <eeit80@312-PC>
2015-08-20 21:02:53 +0800

    AeviewsBean.java
    Add AeviewsBean Field

---
+++ ProjJDBC/src/model/AeviewsBean.java
@@ -0,0 +1,15 @@
+package model;
+
+public class AeviewsBean 
+{
+	private int fullProjId;           //  計畫編號(完整計畫編號) FK
+	private int referMemberId;        //  會員編號(推薦人) FK
+	private int referedMemberId;      //  會員編號(被推薦人) FK
+	private String reviewsContent;    //  評價內容 
+}
+
+
+
+
+
+
