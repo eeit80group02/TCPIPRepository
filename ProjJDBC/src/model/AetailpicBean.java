decf195250b23396d819a80799062238c6dae9fa
finn <finn@eeit-PC>
2015-08-21 12:10:18 +0800

    AetailpicBean.java
    Update Field

--- ProjJDBC/src/model/AetailpicBean.java
+++ ProjJDBC/src/model/AetailpicBean.java
@@ -3,7 +3,7 @@ package model;
 public class AetailpicBean 
 {
 	private int fullProjId;            //  計畫編號(完整計畫編號) FK
-	private int imageName;             //  圖片檔名
+	private String imageName;             //  圖片檔名
 	private Byte[] imageFile;          //  圖檔(檔案)
 	private long imageLength;           //  圖片長度(file)
 	private String imageDescribe;         //  圖片敘述
