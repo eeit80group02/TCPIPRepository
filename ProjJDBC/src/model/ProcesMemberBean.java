248135d7f9d49095ddd03c1f2ac23a6f930528b3
finn <finn@eeit-PC>
2015-08-21 14:13:14 +0800

    ProcesMemberBean.java
    Add ProcesMemberBean Field

---
+++ ProjJDBC/src/model/ProcesMemberBean.java
@@ -0,0 +1,9 @@
+package model;
+
+public class ProcesMemberBean
+{
+	private int planId;     			// 計畫需求編號 FK
+	private int memberId;        	    //會員編號 FK
+	private java.util.Date datetime;    // (指活動發起人下決定跟哪個學校接洽的那刻)(允許空值，只有洽談成功的學校有值)
+}
+
