b71064f9113cd09deabe47eff9273a5df5520473
finn <finn@eeit-PC>
2015-08-26 20:11:31 +0800

    MissionBean.java
    Add getter/setter

--- ProjJDBC/src/model/MissionBean.java
+++ ProjJDBC/src/model/MissionBean.java
@@ -15,11 +15,90 @@ public class MissionBean
 	private	String missionStatus;   // 任務進行狀態(進行中,已完成)
 	private	int mainMissionSetId;   // 主任務編號(「有值」的時候就是子任務) FK
 	
-	private List<MissionParticipatorBean> participator; // 參與者(協助任務執行的人) FK 會員編號
-	
-	
-	
-	
-	
-	
+	public int getMissionBoardId()
+	{
+		return missionBoardId;
+	}
+	public void setMissionBoardId(int missionBoardId)
+	{
+		this.missionBoardId = missionBoardId;
+	}
+	public int getMissionId()
+	{
+		return missionId;
+	}
+	public void setMissionId(int missionId)
+	{
+		this.missionId = missionId;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public int getHost()
+	{
+		return host;
+	}
+	public void setHost(int host)
+	{
+		this.host = host;
+	}
+	public java.util.Date getEndTime()
+	{
+		return endTime;
+	}
+	public void setEndTime(java.util.Date endTime)
+	{
+		this.endTime = endTime;
+	}
+	public int getMissionPriority()
+	{
+		return missionPriority;
+	}
+	public void setMissionPriority(int missionPriority)
+	{
+		this.missionPriority = missionPriority;
+	}
+	public int getBelongMissionSet()
+	{
+		return belongMissionSet;
+	}
+	public void setBelongMissionSet(int belongMissionSet)
+	{
+		this.belongMissionSet = belongMissionSet;
+	}
+	public int getMissionPosition()
+	{
+		return missionPosition;
+	}
+	public void setMissionPosition(int missionPosition)
+	{
+		this.missionPosition = missionPosition;
+	}
+	public String getMissionStatus()
+	{
+		return missionStatus;
+	}
+	public void setMissionStatus(String missionStatus)
+	{
+		this.missionStatus = missionStatus;
+	}
+	public int getMainMissionSetId()
+	{
+		return mainMissionSetId;
+	}
+	public void setMainMissionSetId(int mainMissionSetId)
+	{
+		this.mainMissionSetId = mainMissionSetId;
+	}
+	@Override
+	public String toString()
+	{
+		return "MissionBean [missionBoardId=" + missionBoardId + ", missionId=" + missionId + ", name=" + name + ", host=" + host + ", endTime=" + endTime + ", missionPriority=" + missionPriority + ", belongMissionSet=" + belongMissionSet
+				+ ", missionPosition=" + missionPosition + ", missionStatus=" + missionStatus + ", mainMissionSetId=" + mainMissionSetId + "]";
+	}
 }
