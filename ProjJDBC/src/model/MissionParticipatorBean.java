ae82383968c3b8e829a8684d7e75420d178ccb37
finn <finn@eeit-PC>
2015-08-26 21:08:25 +0800

    Update

--- ProjJDBC/src/model/MissionParticipatorBean.java
+++ ProjJDBC/src/model/MissionParticipatorBean.java
@@ -2,7 +2,7 @@ package model;
 
 public class MissionParticipatorBean 
 {
-	private int fullProjId;    // 計畫編號  FK
-	private int missionSetId;  // 任務編號   FK
+	private int MissionParticipatoId;    // 計畫編號  FK
+	private int missionId;     // 任務編號   FK
 	private int memberId;      // 參與者(協助任務執行的人) FK
 } 
