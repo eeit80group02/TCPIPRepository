118b07da5476a04e75184aa92c7491ac42b70367
finn <finn@eeit-PC>
2015-08-26 20:06:45 +0800

    MissionMessageBean.java
    Update...

--- ProjJDBC/src/model/MissionMessageBean.java
+++ ProjJDBC/src/model/MissionMessageBean.java
@@ -2,8 +2,8 @@ package model;
 
 public class MissionMessageBean 
 {
-	private int fullProjId;                 // 計畫編號(完整計畫編號) FK
-	private int missionId;               // 任務編號 FK
+	private int MissionMessageId;           // 任務留言編號
+	private int missionId;                  // 任務編號 FK
 	private int memberId;                   // 留言者編號(留言者的會員編號) FK
 	private String content;                 // 留言內容
 	private java.util.Date messageTime;     // 留言時間
