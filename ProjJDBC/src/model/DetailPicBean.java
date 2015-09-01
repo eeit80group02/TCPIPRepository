6388b4a36b28c0f336b82e2c62675d63c6498849
doraemon <doraemon0916075265@hotmail.com>
2015-09-01 20:28:53 +0800

    上傳 D 開頭的
    DAOjdbc

--- ProjJDBC/src/model/DetailPicBean.java
+++ ProjJDBC/src/model/DetailPicBean.java
@@ -1,11 +1,50 @@
 package model;
 
-public class DetailPicBean 
-{
-	private int fullProjId;            //  計畫編號(完整計畫編號) FK
-	private String imageName;             //  圖片檔名
-	private Byte[] image;          //  圖檔(檔案)
-	private long imageLength;           //  圖片長度(file)
-	private String imageDescribe;         //  圖片敘述 
-}
+public class DetailPicBean {
+	private int fullProjId; // 計畫編號(完整計畫編號) FK
+	private String imageName; // 圖片檔名
+	private byte[] image; // 圖檔(檔案)
+	private long imageLength; // 圖片長度(file)
+	private String imageDescribe; // 圖片敘述
 
+	public int getFullProjId() {
+		return fullProjId;
+	}
+
+	public void setFullProjId(int fullProjId) {
+		this.fullProjId = fullProjId;
+	}
+
+	public String getImageName() {
+		return imageName;
+	}
+
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+
+	public byte[] getImage() {
+		return image;
+	}
+
+	public void setImage(byte[] image) {
+		this.image = image;
+	}
+
+	public long getImageLength() {
+		return imageLength;
+	}
+
+	public void setImageLength(long imageLength) {
+		this.imageLength = imageLength;
+	}
+
+	public String getImageDescribe() {
+		return imageDescribe;
+	}
+
+	public void setImageDescribe(String imageDescribe) {
+		this.imageDescribe = imageDescribe;
+	}
+
+}
--- ProjJDBC/src/model/DonationDiscussBean.java
+++ ProjJDBC/src/model/DonationDiscussBean.java
@@ -1,19 +1,68 @@
 package model;
 
-public class DonationDiscussBean 
-{
-	private int donationId;       			  // 捐獻編號(對應到【捐獻】表格) FK
-	private String memberMessage;       	  // 會員留言(有意願捐獻者問的問題)
+public class DonationDiscussBean {
+	private int donationId; // 捐獻編號(對應到【捐獻】表格) FK
+	private String memberMessage; // 會員留言(有意願捐獻者問的問題)
 	private java.util.Date memberMessageTime; // 會員留言時間
-	private String schoolMessage;             // 學校留言(學校回覆捐獻者的問題)
+	private String schoolMessage; // 學校留言(學校回覆捐獻者的問題)
 	private java.util.Date schoolMessageTime; // 學校留言時間
-	private int memberId;        	          // 會員編號(提問人的會員編號) FK
-	private int schoolId;       		      // 學校編號(回覆問題的學校編號) FK
+	private int memberId; // 會員編號(提問人的會員編號) FK
+	private int schoolId; // 學校編號(回覆問題的學校編號) FK
+
+	public int getDonationId() {
+		return donationId;
+	}
+
+	public void setDonationId(int donationId) {
+		this.donationId = donationId;
+	}
+
+	public String getMemberMessage() {
+		return memberMessage;
+	}
+
+	public void setMemberMessage(String memberMessage) {
+		this.memberMessage = memberMessage;
+	}
+
+	public java.util.Date getMemberMessageTime() {
+		return memberMessageTime;
+	}
+
+	public void setMemberMessageTime(java.util.Date memberMessageTime) {
+		this.memberMessageTime = memberMessageTime;
+	}
+
+	public String getSchoolMessage() {
+		return schoolMessage;
+	}
+
+	public void setSchoolMessage(String schoolMessage) {
+		this.schoolMessage = schoolMessage;
+	}
+
+	public java.util.Date getSchoolMessageTime() {
+		return schoolMessageTime;
+	}
+
+	public void setSchoolMessageTime(java.util.Date schoolMessageTime) {
+		this.schoolMessageTime = schoolMessageTime;
+	}
+
+	public int getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(int memberId) {
+		this.memberId = memberId;
+	}
+
+	public int getSchoolId() {
+		return schoolId;
+	}
+
+	public void setSchoolId(int schoolId) {
+		this.schoolId = schoolId;
+	}
+
 }
-
-
-
-
-
-
-
