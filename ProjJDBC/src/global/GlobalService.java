3c2930a351b714b77709e38e6fa93a84b58ad156
finn <finn@finn-NB>
2015-09-06 20:25:24 +0800

    Add GlobalService

---
+++ ProjJDBC/src/global/GlobalService.java
@@ -0,0 +1,12 @@
+package global;
+
+public class GlobalService
+{
+	public static final String HOST = "127.0.0.1";
+	public static final String USERNAME = "sasa";
+	public static final String PASSWORD = "passw0rd";
+	public static final String DATABASE = "TCPIP";
+	public static final String URL = "jdbc:sqlserver://" + GlobalService.HOST + ":1433;databaseName=" + GlobalService.DATABASE;
+	
+	
+}
