2b5a93f30fb4e7f27c34216b9e1b4196fac61639
seesawin@gmail.com <seesaw1988>
2015-09-20 19:49:56 +0800

    wei wei
    donation update page/dCart

---
+++ WebServletJdbc/WebContent/donationScripts/DonationWallHead.js
@@ -0,0 +1,46 @@
+$(function() {
+	// searchIcon
+	$("#searchIcon").hover(function() {
+		$("#searchIcon").css("cursor", "pointer");
+	});
+	$("#searchIcon").click(function() {
+		// 按 searchIcon 搜尋
+		var input = $("#searchDonation").val();
+		if (input != null && input.trim().length != 0) {
+			$("#searchDonation").val("" + input);
+//			alert("按 Icon 搜尋 " + input);
+		}
+	});
+
+	// clearIcon
+	$("#clearIcon").hover(function() {
+		$("#clearIcon").css("cursor", "pointer");
+	});
+
+	$("#clearIcon").click(function() {
+		// 按 clearIcon 清除
+		$("#searchDonation").val("");
+	});
+
+	$("#searchDonation").keydown(function(evt) {
+		// 按Enter 搜尋
+		if (evt.keyCode == 13) {
+			var input = $("#searchDonation").val();
+			if (input != null && input.trim().length != 0) {
+				$("#searchDonation").val("" + input);
+//				alert("按Enter 搜尋 " + input);
+			}
+		}
+	});
+
+	$(".chooseItem").click(getPressValue);
+
+	$(".chooseDropdownItem").click(getPressValue);
+	
+	function getPressValue(){
+		var input = $(this).attr("value");
+//		alert("以 " + input + " 排序");
+	}
+	
+	
+}(jQuery));
