908c68c98d5f385bc947eca5ac0d51042b3d8fff
finn <finn@finn-NB>
2015-09-22 21:24:12 +0800

    參加者 service

---
+++ WebServletJdbc/src/model/service/ParticipatorService.java
@@ -0,0 +1,26 @@
+package model.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.ParticipatorBean;
+
+public class ParticipatorService
+{
+	public List<ParticipatorBean> selet(int fullProjId)
+	{
+		List<ParticipatorBean> result = new ArrayList<ParticipatorBean>();
+		
+		return result;
+	}
+	 
+	public ParticipatorBean Participate(ParticipatorBean bean)
+	{
+		return bean;
+	}
+	public static void main(String[] args)
+	{
+
+	}
+
+}
