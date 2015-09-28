65a44f1cb9caf53fed72ecef2b5b9f6dceed6ddf
user <user@doraemon>
2015-09-28 23:00:15 +0800

    dora 0928 1100

--- WebServletJdbc/WebContent/donationScripts/DonationBill.js
+++ WebServletJdbc/WebContent/donationScripts/DonationBill.js
@@ -22,79 +22,18 @@ $(window).load(function() {
 		$(this).trigger($.Event("click"));
 		Materialize.toast('<i class="tiny material-icons">info_outline</i>&nbsp;<span>按 Esc 或 <i class="tiny material-icons">done</i> 關閉視窗</span>', 2000, 'rounded');
 	});
-	// 第三頁的小叮嚀
 	$("#bodyContent button[data-target='modalNote03']").one('mouseover', function() {
 		// 第一次移過去等同 click
 		$(this).trigger($.Event("click"));
 		Materialize.toast('<i class="tiny material-icons">info_outline</i>&nbsp;<span>按 Esc 或 <i class="tiny material-icons">done</i> 關閉視窗</span>', 2000, 'rounded');
 	});
-
-	// 換頁系列 start
-	// var $body = (window.opera) ? (document.compatMode == "CSS1Compat" ?
-	// $('html') : $('body')) : $('html,body');
-	//
-	// $("#test1 #page01Next").click(function() {
-	// // 開二
-	// $(".tabs li[id=pageTab02]").attr("class", "tab col s3");
-	// $("#pageTab02 a").trigger($.Event("click"));
-	// // 關一
-	// $(".tabs li[id=pageTab01]").attr("class", "tab col s3 disabled");
-	//
-	// $body.animate({
-	// scrollTop : 0
-	// }, 600);
-	// return false;
-	// });
-	//
-	// $("#test2 #page02Prev").click(function() {
-	// // 開一
-	// $(".tabs li[id=pageTab01]").attr("class", "tab col s3");
-	// $("#pageTab01 a").trigger($.Event("click"));
-	// // 關二
-	// $(".tabs li[id=pageTab02]").attr("class", "tab col s3 disabled");
-	//
-	// $body.animate({
-	// scrollTop : 0
-	// }, 600);
-	// return false;
-	// });
-	// $("#test2 #page02Next").click(function() {
-	// // 開三
-	// $(".tabs li[id=pageTab03]").attr("class", "tab col s3");
-	// $("#pageTab03 a").trigger($.Event("click"));
-	//
-	// $(".tabs li[id=pageTab02]").attr("class", "tab col s3 disabled");
-	// // 關二
-	// $body.animate({
-	// scrollTop : 0
-	// }, 600);
-	// return false;
-	// });
-	// $("#test3 #page03Prev").click(function() {
-	// // 開二
-	// $(".tabs li[id=pageTab02]").attr("class", "tab col s3");
-	// $("#pageTab02 a").trigger($.Event("click"));
-	// // 關三
-	// $(".tabs li[id=pageTab03]").attr("class", "tab col s3 disabled");
-	//
-	// $body.animate({
-	// scrollTop : 0
-	// }, 600);
-	// return false;
-	// });
-	// $("#test3 #btnSend").click(function() {
-	// // 開四
-	// $(".tabs li[id=pageTab04]").attr("class", "tab col s3");
-	// $("#pageTab04 a").trigger($.Event("click"));
-	// // 關三
-	// $(".tabs li[id=pageTab03]").attr("class", "tab col s3 disabled");
-	//
-	// $body.animate({
-	// scrollTop : 0
-	// }, 600);
-	// return false;
-	// });
-	// 換頁系列 end
+	
+	// 第三頁的小叮嚀
+	$("#test3 button[data-target='modalNote04']").one('mouseover', function() {
+		// 第一次移過去等同 click
+		$(this).trigger($.Event("click"));
+		Materialize.toast('<i class="tiny material-icons">info_outline</i>&nbsp;<span>按 Esc 或 <i class="tiny material-icons">done</i> 關閉視窗</span>', 2000, 'rounded');
+	});
 
 	// 同步會員資料
 	$("#test2 #buttonRight01").click(function() {
