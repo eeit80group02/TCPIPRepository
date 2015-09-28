e52ba4866f2904a6d171a674eeae042d19c6bff5
delphi <delphi@delphi-PC>
2015-09-28 08:16:27 +0800

    FulProj FakeInfos Test

---
+++ WebServletJdbc/src/_init/InsertFakeInfos.java
@@ -0,0 +1,61 @@
+package _init;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import global.GlobalService;
+@WebServlet("/fakeInfos.do")
+public class InsertFakeInfos extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		request.setCharacterEncoding("UTF-8");
+		String content = request.getParameter("projAbstract");
+		
+		String updateToDataBase = "insert into fulProj(Content) values(?)";
+		
+		
+		try{
+			Context context = new InitialContext();
+			
+			DataSource datasorce = (DataSource)context.lookup(GlobalService.JNDI);
+			Connection conn = datasorce.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement(updateToDataBase);
+			pstmt.setString(1, content);
+			pstmt.execute();
+			
+			pstmt.close();
+			conn.close();
+			
+		}catch(SQLException | NamingException e){
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	
+	
+	
+	
+}
