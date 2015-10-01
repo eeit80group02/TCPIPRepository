16aa871464de60bd751f104e92b17bbfd344e402
Yang <Student@III-EEIT>
2015-10-01 09:30:43 +0800

    Yang

--- WebServletJdbc/src/index/filter/IndexFilter.java
+++ WebServletJdbc/src/index/filter/IndexFilter.java
@@ -1,39 +1,41 @@
-package index.filter;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Enumeration;
-
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
-import javax.servlet.annotation.WebInitParam;
-
-@WebFilter(
-		urlPatterns={"/*"},
-		initParams = {
-			@WebInitParam(name="index", value="/index.jsp")
-		})
-public class IndexFilter  implements Filter{
-	Collection<String> url = new ArrayList<String>();
-	
-	@Override
-	public void init(FilterConfig fConfig) throws ServletException {
-		Enumeration<String> e = fConfig.getInitParameterNames();
-		while(e.hasMoreElements()){
-			String name = e.nextElement();
-			url.add(fConfig.getInitParameter(name));
-		}
-	}
-
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-			throws IOException, ServletException {
+//package index.filter;
+//
+//import java.io.IOException;
+//import java.util.ArrayList;
+//import java.util.Collection;
+//import java.util.Enumeration;
+//
+//import javax.servlet.Filter;
+//import javax.servlet.FilterChain;
+//import javax.servlet.FilterConfig;
+//import javax.servlet.ServletException;
+//import javax.servlet.ServletRequest;
+//import javax.servlet.ServletResponse;
+//import javax.servlet.annotation.WebFilter;
+//import javax.servlet.annotation.WebInitParam;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//@WebFilter(
+//		urlPatterns={"/*"},
+//		initParams = {
+//			@WebInitParam(name="index", value="/index.jsp")
+//		})
+//public class IndexFilter  implements Filter{
+//	Collection<String> url = new ArrayList<String>();
+//	
+//	@Override
+//	public void init(FilterConfig fConfig) throws ServletException {
+//		Enumeration<String> e = fConfig.getInitParameterNames();
+//		while(e.hasMoreElements()){
+//			String name = e.nextElement();
+//			url.add(fConfig.getInitParameter(name));
+//		}
+//	}
+//
+//	@Override
+//	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+//			throws IOException, ServletException {
 //		if(request instanceof HttpServletRequest && response instanceof HttpServletResponse){
 //			HttpServletRequest req = (HttpServletRequest) request;
 //			HttpServletResponse resp = (HttpServletResponse) response;
@@ -49,11 +51,13 @@ public class IndexFilter  implements Filter{
 //		} else {
 //			throw new ServletException("Request/Response Type Error");
 //		}
-	}
-
-
-	@Override
-	public void destroy() {
-		
-	}
-}
+//		
+////		chain.doFilter(request, response);
+//	}
+//
+//
+//	@Override
+//	public void destroy() {
+//		
+//	}
+//}
